8883f72ce44c0c17a18865126c99664c49f7f79a
jadhavakshay29 <akshayjadhavaj2911@gmail.com>
2023-03-13 21:03:20 +0530

    added counter

--- src/Components/UpdatingState/UpdatingState.jsx
+++ src/Components/UpdatingState/UpdatingState.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const UpdatingState = ()=>{
+
+    let [counter, setCounter] = useState(0);
+    const onIncrement = (event)=>{
+        event.preventDefault();
+        setCounter(counter+1);  //count++ doesnt work in react count+1 does
+        
+    }
+
     return (
         <div>
-            <h3></h3>
-            <button>Update counter</button>
+            <h3>{counter}</h3>
+            <button onClick={onIncrement}>Update counter</button>
         </div>
     )
 }
